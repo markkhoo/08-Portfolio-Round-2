aa3a5d071467399a602e7a224889e389bb7ca1e3
markeezy <markkhoo95@gmail.com>
2021-04-12 17:43:58 -0700

    add repos and view functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,11 @@
+// Pointers
+var aboutContainer = document.getElementById("about");
+var work_Container = document.getElementById("work_");
+var reposContainer = document.getElementById("repos");
+var contaContainer = document.getElementById("conta");
 
+// Global Variables
+var repos = [];
 
 // Display for time since started coding
 let timeSince = document.getElementById("time-since");
@@ -7,3 +14,75 @@ setInterval(function() {
     timeSince.innerText = time.toString().substring(0,9);
 },50);
 
+// Fetch Reops
+let requestURL = 'https://api.github.com/users/markkhoo/repos';
+fetch(requestURL)
+.then(response => {
+    return response.json();
+})
+.then(function(data){
+    repos = data;
+    displayRepos();
+});
+
+// Display GitHub Repos
+function displayRepos() {
+    reposContainer.innerHTML = "";
+
+    if (repos.length != 0) {
+        for (let i = 0; i < repos.length; i++) {
+            let singleRepo = document.createElement("a");
+            singleRepo.innerHTML = repos[i].name;
+            singleRepo.setAttribute("href", repos[i].html_url);
+            singleRepo.setAttribute("target","_blank");
+            singleRepo.setAttribute("rel", "noreferrer noopener");
+
+            reposContainer.appendChild(singleRepo);
+        };
+    } else {
+        let noRepos = document.createElement("h4");
+        noRepos.innerHTML = "No Public Repos";
+        reposContainer.appendChild(noRepos);
+    };
+};
+
+// INITIALIZE
+function init () {
+    aboutContainer.style.setProperty("display","initial");
+    work_Container.style.setProperty("display","none");
+    reposContainer.style.setProperty("display","none");
+    contaContainer.style.setProperty("display","none");
+};
+init();
+
+// Click About Me
+document.getElementById("click_about").addEventListener("click",function(){
+    aboutContainer.style.setProperty("display","initial");
+    work_Container.style.setProperty("display","none");
+    reposContainer.style.setProperty("display","none");
+    contaContainer.style.setProperty("display","none");
+});
+
+// Click Work
+document.getElementById("click_work_").addEventListener("click",function(){
+    aboutContainer.style.setProperty("display","none");
+    work_Container.style.setProperty("display","initial");
+    reposContainer.style.setProperty("display","none");
+    contaContainer.style.setProperty("display","none");
+});
+
+// Click Github Repos
+document.getElementById("click_repos").addEventListener("click",function(){
+    aboutContainer.style.setProperty("display","none");
+    work_Container.style.setProperty("display","none");
+    reposContainer.style.setProperty("display","initial");
+    contaContainer.style.setProperty("display","none");
+});
+
+// Click Contact
+document.getElementById("click_conta").addEventListener("click",function(){
+    aboutContainer.style.setProperty("display","none");
+    work_Container.style.setProperty("display","none");
+    reposContainer.style.setProperty("display","none");
+    contaContainer.style.setProperty("display","initial");
+});
